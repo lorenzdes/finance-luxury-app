9eba3854d697e047b4c1c5f4c5551fea27f11ac6
lorenzdes <lorello97@gmail.com>
2024-07-09 16:29:47 +0200

    graphics

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <template>
   <v-app>
-    <v-app-bar app>
+    <v-app-bar app color="deep-purple darken-2" dark>
       <v-toolbar-title>
         <RouterLink to="/">
-          Vue Luxe Life
+          <v-img
+            src="/Black And White Modern Vintage Retro Brand Logo.png"
+            alt="Vue Luxe Life Logo"
+            contain
+          ></v-img>
         </RouterLink>
       </v-toolbar-title>
-      <v-spacer></v-spacer>      
-      <v-btn to="/login" text>Login</v-btn>
-      <v-btn to="/" text>Feed</v-btn>
-      <v-btn to="/financial" text>Financial Planning</v-btn>
-      <v-btn to="/luxury" text>Luxury Discovery</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn prepend-icon="mdi-login-variant" to="/login" text class="white--text">Login</v-btn>
+      <v-btn prepend-icon="mdi-rss" to="/" text class="white--text">Feed</v-btn>
+      <v-btn prepend-icon="mdi-chart-line" to="/financial" text class="white--text">Financial Planning</v-btn>
+      <v-btn prepend-icon="mdi-diamond" to="/luxury" text class="white--text">Luxury Discovery</v-btn>
     </v-app-bar>
     <v-main>
       <router-view></router-view>
     </v-main>
+    <v-footer class="bg-grey-lighten-1">
+    <v-row justify="center" no-gutters>
+      <v-col class="text-center mt-4" cols="12">
+        {{ new Date().getFullYear() }} — <strong>LUX LIFE</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
   </v-app>
 </template>
 
